12018830cef613b32946639516b64add4151b0ed
SamGold04 <115782074+SamGold04@users.noreply.github.com>
2022-11-02 11:45:17 +0000

    0.0.1
    spawns in random pos

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public int randomNumber = Random.Range(0, 9);
 public class Spawn : MonoBehaviour
 {
-
+    //This Scipt is for assigning a random starting location for the player
     void Start()
     {
-        
+        int randomNumber = Random.Range(-9, 9); //random number between -9 and 9
+        transform.position = new Vector3(randomNumber, 0, 0); //transforms to x coord of random number
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(0, randomNumber, 0);
+        
     }
 }
