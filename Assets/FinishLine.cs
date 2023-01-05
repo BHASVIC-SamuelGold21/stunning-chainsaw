d76a681a4395068cf5e86f85a8efbe039cf81486
BHASVIC-SamuelGold21 <115782074+BHASVIC-SamuelGold21@users.noreply.github.com>
2023-01-05 17:30:24 +0000

    times enemy cross finishline are recorded
    next time finish the insertion sort algorythm at the moment im pretty sure it doesnt run

--- Assets/FinishLine.cs
+++ Assets/FinishLine.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-public Transform finish; // get transform component
-public Material transparentMat; //transparent material for finish
-public float levelTimer = 0; //public runtime for dataManager
-private bool updateTimer = false;
-
-public bool passFinish = true;
-private Renderer ren; //renderer componentA
-void Start() {
-    ren = GetComponent<Renderer>();
-    updateTimer = true;
-    levelTimer = 0.0f;
-    passFinish = false;
-}
-void Update() //update is called every frame
-{ 
-    if(updateTimer == true)
-    {
-        levelTimer += 1* Time.deltaTime;
-
+    public Transform finish; // get transform component
+    public Material transparentMat; //transparent material for finish
+    public float levelTimer = 0; //public runtime for dataManager
+    private bool updateTimer = false;
+    public bool passFinish = false;
+    private bool finalTimeRecorded;
+    private Renderer ren; //renderer componentA
+    private int i = 1;
+    void Start() {
+        ren = GetComponent<Renderer>();
+        updateTimer = true;
+        levelTimer = 0.0f;
+        passFinish = false;
     }
-    if(transform.position.y <= finish.position.y) //when the enemy position is below finish line on the y axis
-    {
+    void Update() //update is called every frame
+    {    
+        
+        if(!passFinish)
+        {
+            levelTimer += 1* Time.deltaTime;
+
+        }
+        if(transform.position.y <= finish.position.y && !finalTimeRecorded) //when the enemy position is below finish line on the y axis
+        {
+            passFinish = true;
+            finalTimeRecorded = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
         ren.material = transparentMat; //changes the material to invisible
-        passFinish = true;
     }
-}
 }
