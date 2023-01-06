a07997aae806b7543160ac309891f9eb3527a7cf
SamGold04 <115782074+SamGold04@users.noreply.github.com>
2023-01-06 08:58:28 +0000

    Revert "Revert "see if this fix""
    This reverts commit 1cebe96cf14f3cf10f68ffc223b80eec891280fd.

--- Assets/RunTime.cs
+++ Assets/RunTime.cs
@@ -6,21 +6,19 @@ public class RunTime : MonoBehaviour
 {
 float timerInSecond = 0.0f;
 private float levelTimer = 0.0f;
-public float totalTimer;
 private bool updateTimer = false;
-    // Start is called before the first frame update
+    //Start is called before the first frame update
     void Start()
     {
-        updateTimer = true;
+        updateTimer = true; //set values for when script becomes non-static
         levelTimer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(updateTimer == true){
-            levelTimer += 1* Time.deltaTime;
+        if(updateTimer == true){ //while true the timer will count up
+            levelTimer += 1* Time.deltaTime; 
         }
-
     }
 }
